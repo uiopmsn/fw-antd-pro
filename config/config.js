774830c1a4760268d3bf229375b687448883bda7
uiopmsn <54045139@qq.com>
2020-02-10 23:04:09 +0800

    1.0

--- config/config.js
+++ config/config.js
@@ -4,9 +4,10 @@ import slash from 'slash2';
 import themePluginConfig from './themePluginConfig';
 const { pwa } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
-
+/*
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
+*/
 const plugins = [
   ['umi-plugin-antd-icon-config', {}],
   [
@@ -54,7 +55,7 @@ const plugins = [
     },
   ],
 ];
-
+/*
 if (isAntDesignProPreview) {
   // 针对 preview.pro.ant.design 的 GA 统计代码
   plugins.push([
@@ -65,7 +66,7 @@ if (isAntDesignProPreview) {
   ]);
   plugins.push(['umi-plugin-antd-theme', themePluginConfig]);
 }
-
+*/
 export default {
   plugins,
   hash: true,
@@ -136,8 +137,8 @@ export default {
     // ...darkTheme,
   },
   define: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
-      ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: ''
+      //ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
   },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
